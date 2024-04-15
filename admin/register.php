722f9340967d9fd940e1b9fa451fb4db1f549487
wissem2908 <115625112+wissem2908@users.noreply.github.com>
2024-04-15 11:26:01 +0200

    add footer page in admin

--- admin/register.php
+++ admin/register.php
@@ -132,75 +132,10 @@ include ('includes/header_1.php');
            
         </div>
     </div>
-    <footer >
-      
-      <div class="footer-bottom text-center ">
+    <?php
+    include ("includes/footer_1.php");
 
-              <div  class="col-sm-12" >
-              <ul class="footer-nav">
-                  <li >
-                      <a  href="terms_conditions.php">Terms & Conditions</a>
-                  </li>
-                  <li class="nav-item">
-                      <a  href="privacy_policy.php">Privacy Policy</a>
-                  </li>
-                
-              </ul>
-             
-          </div>
-           <p class="mb-0"><a href="/"  style='color:white;text-decoration: none;'>Copyright © <?php echo $website; ?> 2023</a></p>
-      </div>
-  </footer>
-
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered modal-xl">
-          <div class="modal-content">
-              <div class="modal-body p-0">
-                  <div class="container-fluid">
-                      <div class="row gy-4">
-                          <div class="col-lg-4 col-sm-12 bg-cover"
-                              style="background-image: url(img/c2.jpg); min-height:300px;">
-                              <div>
-                                  
-                              </div>
-                          </div>
-                          <div class="col-lg-8">
-                              <form class="p-lg-5 col-12 row g-3">
-                                  <div>
-                                      <h1>Get in touch</h1>
-                                  <p>Fell free to contact us and we will get back to you as soon as possible</p>
-                                  </div>
-                                  <div class="col-lg-12">
-                                      <label for="fullName" class="form-label">Full name</label>
-                                      <input type="text" class="form-control" id="fullName"
-                                          aria-describedby="emailHelp">
-                                  </div>
-                                
-                                  <div class="col-12">
-                                      <label for="email" class="form-label">Email address</label>
-                                      <input type="email" class="form-control"  id="email"
-                                          aria-describedby="emailHelp">
-                                  </div>
-                                  <div class="col-12">
-                                      <label for="" class="form-label">Enter Message</label>
-                                      <textarea name="message"  class="form-control" id="message"  rows="4"></textarea>
-                                  </div>
-
-                                  <div class="col-12">
-                                      <button type="button" class="btn btn-brand" id="send">Send Message</button>
-                                  </div>
-                              </form>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-
-          </div>
-      </div>
-  </div>
-
-
+    ?>
 
 <!--**********************************
 	Scripts
